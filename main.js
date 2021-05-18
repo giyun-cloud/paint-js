2463de8c5bbc99dcd47bed72928d0a5a3d0546ec
giyun-cloud <nangiyoon@gmail.com>
2021-05-18 21:10:05 +0900

    Make save feature

--- main.js
+++ main.js
@@ -1,15 +1,23 @@
 const canvas = document.getElementById('jsCanvas')
 const colorBtns = document.querySelectorAll('li.color')
+const range = document.getElementById('jsRange')
+const modeBtn = document.getElementById('jsMode')
+
+//COMMON
+const canvasSize = 600
+const colorInitial = '#2c2c2c'
+const lineWidthInitial = range.value
 
 let paintingCan = false
+let changedMode = false
+canvas.width = canvasSize
+canvas.height = canvasSize
+
 const ctx = canvas.getContext('2d')
 
-ctx.setStrokeColor('rgb(200,50,50)')
-ctx.lineWidth = 2.5
-canvas.width = 600
-canvas.height = 600
-
-console.log(ctx.strokeStyle)
+ctx.lineWidth = lineWidthInitial
+ctx.strokeStyle = colorInitial
+ctx.fillStyle = colorInitial
 
 function stopPainting() {
   paintingCan = false
@@ -17,7 +25,7 @@ function stopPainting() {
 
 function onMouseMove(event) {
   const { offsetX: x, offsetY: y } = event
-  if (paintingCan) {
+  if (paintingCan && !changedMode) {
     ctx.lineTo(x, y)
     ctx.stroke()
   }
@@ -26,23 +34,48 @@ function onMouseMove(event) {
 function onMouseDown() {
   paintingCan = true
   ctx.beginPath()
+  if (changedMode) {
+    ctx.fillStyle === ctx.strokeStyle || (ctx.fillStyle = ctx.strokeStyle)
+    ctx.fillRect(0, 0, canvas.width, canvas.height)
+  }
 }
 
 function onMouseUp() {
   stopPainting()
 }
 
-function changeColor(index) {}
+function changeColor(e) {
+  ctx.strokeStyle = getComputedStyle(e.target).backgroundColor
+}
+
+function changeLineWidth(e) {
+  ctx.lineWidth = e.target.value
+}
+
+function changeModeFunc() {
+  // changedMode 변수와 헷갈리지않게 Func추가함
+  if (changedMode) {
+    changedMode = false
+    modeBtn.innerText = 'fill'
+  } else {
+    changedMode = true
+    modeBtn.innerText = 'paint'
+  }
+}
 
 function init() {
   canvas.addEventListener('mousemove', onMouseMove)
   canvas.addEventListener('mousedown', onMouseDown)
   canvas.addEventListener('mouseup', onMouseUp)
   canvas.addEventListener('mouseleave', stopPainting)
-  for (let index = 0; index < colorBtns.length; index++) {
-    colorBtns[index].addEventListener('click', () => {
-      changeColor(index)
-    })
-  }
+  // 위4개는 모두 canvas에서 첫번째 인자의 상태일때만 호출된다. 고로 canvas안에서만 일어남.
+
+  colorBtns.forEach((colorBtn, index) => {
+    colorBtn.addEventListener('click', changeColor)
+  })
+
+  range.addEventListener('input', changeLineWidth)
+
+  modeBtn.addEventListener('click', changeModeFunc)
 }
 init()
